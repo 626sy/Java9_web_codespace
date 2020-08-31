b49eddddff61e3f6c9fff0ee876430fb941805ac
石浩博 <42900775+626sy@users.noreply.github.com>
2020-08-31 09:20:27 +0800

    注册

---
+++ day04_Cookies_Session/src/com/neusoft/servlet/RegisterServlet.java
@@ -0,0 +1,28 @@
+package com.neusoft.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author shihaobo
+ * @date 2020/8/31 9:16
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String gender = req.getParameter("gender");
+    }
+}
