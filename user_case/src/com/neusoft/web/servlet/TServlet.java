e6d8066a6080487f074ea36a04ae8766ec27a19b
石浩博 <42900775+626sy@users.noreply.github.com>
2020-09-01 14:56:38 +0800

    servlet模板

---
+++ user_case/src/com/neusoft/web/servlet/TServlet.java
@@ -0,0 +1,23 @@
+package com.neusoft.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author shihaobo
+ * @date 2020/9/1 14:55
+ */
+public class TServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+}
